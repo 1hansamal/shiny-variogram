0b07dd9a71a6c34b6bc16591a548f4905c48cb26
1hansamal <ihanzamal@gmail.com>
2026-07-16 20:51:13 +0530

    add packages

---
+++ app/global.R
@@ -0,0 +1,15 @@
+library(shiny)
+library(shinyjs)
+
+library(bslib)
+library(bsicons)
+
+library(DT)
+
+library(data.table)
+
+library(sf)
+library(gstat)
+
+# set size of file uploads to 10MB
+options(shiny.maxRequestSize = 10 * 1024^2)
