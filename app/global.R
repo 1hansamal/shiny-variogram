a5f047919fd3256ac7b0a18dea7e8c28f69e963e
1hansamal <ihanzamal@gmail.com>
2026-07-17 00:38:13 +0530

    helper func to read uploaded files

--- app/global.R
+++ app/global.R
@@ -16,3 +16,31 @@ options(shiny.maxRequestSize = 10 * 1024^2)
 
 testData <- st_read(here::here("app/test/data/meuse.geojson"), quiet = TRUE)
 
+myTheme <- bs_theme(version = 5, bootswatch = "flatly") #|>
+# bs_add_variables(here::here("app/www/variables.scss")) |>
+# bs_add_rules(here::here("app/www/rules.scss"))
+
+##' read the given file and return it
+ReadFile <- function(filePath, fileType) {
+  if (fileType == "shp") {
+    tempDir <- tempdir()
+    utils::unzip(filePath, exdir = tempDir, overwrite = TRUE)
+
+    # find the .shp file
+    shpFiles <- list.files(tempDir, pattern = "\\.shp$", full.names = TRUE)
+    if (length(shpFiles) == 0) {
+      stop("No .shp file found in the ZIP archive")
+    }
+
+    # this only give the first found file in alphabetical order !!
+    shpPath <- shpFiles[1]
+    data <- st_read(shpPath, quiet = TRUE)
+
+  } else if (fileType == "geojson") {
+    data <- st_read(filePath, quiet = TRUE)
+  } else if (fileType == "csv") {
+    data <- fread(filePath)
+  }
+
+  return(data)
+}
