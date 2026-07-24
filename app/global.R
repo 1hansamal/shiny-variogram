92b83547a746aed513cc695a15a4be49cee5cb53
1hansamal <ihanzamal@gmail.com>
2026-07-24 12:42:17 +0530

    ReadFIle is rewritten with switch

--- app/global.R
+++ app/global.R
@@ -1,5 +1,4 @@
 library(shiny)
-library(shinyjs)
 
 library(bslib)
 library(bsicons)
@@ -14,32 +13,42 @@ library(gstat)
 # set size of file uploads to 10MB
 options(shiny.maxRequestSize = 10 * 1024^2)
 
-testData <- st_read(here::here("app/test/data/meuse.geojson"), quiet = TRUE)
+test_data <- st_read(here::here("app/test/data/meuse.geojson"), quiet = TRUE)
 
 myTheme <- bs_theme(version = 5, bootswatch = "flatly") #|>
 # bs_add_variables(here::here("app/www/variables.scss")) |>
 # bs_add_rules(here::here("app/www/rules.scss"))
 
 # read the given file and return it
-ReadFile <- function(filePath, fileType) {
-  if (fileType == "shp") {
-    tempDir <- tempdir()
-    utils::unzip(filePath, exdir = tempDir, overwrite = TRUE)
+ReadFile <- function(file_path, file_type) {
+  data <- switch(
+    file_type,
 
-    # find the .shp file
-    shpFiles <- list.files(tempDir, pattern = "\\.shp$", full.names = TRUE)
-    if (length(shpFiles) == 0) {
-      stop("No .shp file found in the ZIP archive")
-    }
+    zip = {
+      temp_dir <- tempdir()
+      utils::unzip(file_path, exdir = temp_dir, overwrite = TRUE)
 
-    # this only give the first found file in alphabetical order !!
-    shpPath <- shpFiles[1]
-    data <- st_read(shpPath, quiet = TRUE)
-  } else if (fileType == "geojson") {
-    data <- st_read(filePath, quiet = TRUE)
-  } else if (fileType == "csv") {
-    data <- read.csv(filePath)
-  }
+      # Find the .shp file
+      shp_files <- list.files(temp_dir, pattern = "\\.shp$", full.names = TRUE)
+      if (length(shp_files) == 0) {
+        stop("No .shp file found in the ZIP archive")
+      }
+
+      # Uses the first .shp file found
+      shp_path <- shp_files[1]
+      st_read(shp_path, quiet = TRUE)
+    },
+
+    geojson = {
+      st_read(file_path, quiet = TRUE)
+    },
+
+    csv = {
+      read.csv(file_path)
+    },
+
+    stop(sprintf("Unsupported file type: %s", file_type))
+  )
 
   return(data)
 }
@@ -71,40 +80,47 @@ MakeBoundaries <- function(
     "ward"
   ),
   nbins = NULL,
-  cutoff = NULL
+  cutoff = 0.5
 ) {
   method <- match.arg(method)
-
+  
   dmax <- max(distances)
-  if (is.null(cutoff) || cutoff > dmax) {
-    cutoff <- dmax
-  }
+  cutoff <- if (cutoff <= 1) dmax * cutoff else min(cutoff, dmax)
 
   d <- distances[distances <= cutoff]
 
-  if (is.null(nbins)) {
+  # Methods that require user-specified nbins
+  if (method %in% c("even", "uniform", "kmeans", "ward")) {
+
+    nbins <- as.integer(nbins)
+  } else {
     nbins <- switch(
       method,
       fd = ceiling(cutoff / method.fd(d)),
-      sturges = method.sturges(d),
       scott = ceiling(cutoff / method.scott(d)),
       rice = method.rice(d),
-      sqrt = method.sqrt(d),
+      sturges = method.sturges(d),
       doane = method.doane(d),
-      kmeans = method.sqrt(d),
-      ward = method.sqrt(d),
-      even = method.sqrt(d),
-      uniform = method.sqrt(d)
+      sqrt = method.sqrt(d)
     )
-  }
 
-  nbins <- max(1, as.integer(nbins))
+    nbins <- max(1L, nbins)
+  }
 
   boundaries <- switch(
     method,
     even = seq(0, cutoff, length.out = nbins + 1),
-    uniform = method.uniform(d, nbins, cutoff),
-    seq(0, cutoff, length.out = nbins + 1)
+    uniform = method.uniform(d, nbins),
+    kmeans = method.kmeans(d, nbins),
+    ward = method.ward(d, nbins),
+
+    # Histogram-based methods compute nbins automatically
+    fd = seq(0, cutoff, length.out = nbins + 1),
+    scott = seq(0, cutoff, length.out = nbins + 1),
+    rice = seq(0, cutoff, length.out = nbins + 1),
+    sturges = seq(0, cutoff, length.out = nbins + 1),
+    doane = seq(0, cutoff, length.out = nbins + 1),
+    sqrt = seq(0, cutoff, length.out = nbins + 1)
   )
 
   boundaries
@@ -113,35 +129,29 @@ MakeBoundaries <- function(
 
 method.uniform <- function(d, nbins) {
   probs <- seq(0, 1, length.out = nbins + 1)
-  as.numeric(quantile(d, probs = probs, na.rm = TRUE))
-}
+  bounds <- unique(as.numeric(quantile(d, probs, na.rm = TRUE)))
 
+  if (length(bounds) < 2) {
+    bounds <- c(min(d), max(d))
+  }
+  bounds
+}
 
 method.fd <- function(d) {
-  h <- 2 * IQR(d) / length(d)^(1 / 3)
+  2 * IQR(d) / length(d)^(1 / 3)
 }
-
-
 method.sturges <- function(d) {
   ceiling(log2(length(d)) + 1)
 }
-
-
 method.scott <- function(d) {
-  h <- sd(d) * (24 * sqrt(pi) / length(d))^(1 / 3)
+  sd(d) * (24 * sqrt(pi) / length(d))^(1 / 3)
 }
-
-
 method.rice <- function(d) {
   ceiling(2 * length(d)^(1 / 3))
 }
-
-
 method.sqrt <- function(d) {
   ceiling(sqrt(length(d)))
 }
-
-
 method.doane <- function(d) {
   n <- length(d)
   s <- sd(d)
@@ -156,8 +166,6 @@ method.doane <- function(d) {
 
   ceiling(1 + log2(n) + log2(1 + abs(g1) / sg1))
 }
-
-
 method.kmeans <- function(distances, nbins, cutoff) {
   if (nbins == 1) {
     return(c(0, cutoff))
@@ -171,8 +179,6 @@ method.kmeans <- function(distances, nbins, cutoff) {
 
   bounds
 }
-
-
 method.ward <- function(distances, nbins, cutoff, agg = c("mean", "median")) {
   agg <- match.arg(agg)
 
@@ -205,3 +211,17 @@ method.ward <- function(distances, nbins, cutoff, agg = c("mean", "median")) {
 
   c(0, (centers[-1] + centers[-length(centers)]) / 2, cutoff)
 }
+
+
+binning_methods <- c(
+  "Even Width" = "even",
+  "Uniform Count (quantile)" = "uniform",
+  "Freedman-Diaconis" = "fd",
+  "Scott" = "scott",
+  "Sturges" = "sturges",
+  "Rice" = "rice",
+  "Doane" = "doane",
+  "Square Root" = "sqrt",
+  "K-Means Clustering" = "kmeans",
+  "Ward Clustering" = "ward"
+)
